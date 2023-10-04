624b0e3b1c678af882d00729944d7198204a0b0e
Erlkonig <vl.piatachenko@cs.sumdu.edu.ua>
2023-10-05 00:01:46 +0300

    Revert "local fix"
    This reverts commit 2a0a6ff7a469e16d0daab07d03e7097054926999.

--- WebStudent/src/main/java/sumdu/edu/ua/webstudent/StudentAdd.java
+++ WebStudent/src/main/java/sumdu/edu/ua/webstudent/StudentAdd.java
@@ -1,44 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package sumdu.edu.ua.webstudent;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  *
- * @author Erlkonig
+ * @author Oksana
  */
-@WebServlet(name = "StudentAdd", urlPatterns = {"/StudentAdd"})
-public class StudentAdd extends HttpServlet {
+public class AddStudent extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    
-    HttpSession session = request.getSession();
-    List<Student> students = (List<Student>)session.getAttribute("students");
-    
-    if (students == null) {
-        students = new LinkedList<Student>();
-        session.setAttribute("students", students);
-    }
-    
-    if (request.getParameter("name") != "" || request.getParameter("surname") != "") {
-        Student student = new Student (request.getParameter("name"),
-                                       request.getParameter("surname"),
-                                       request.getParameter("email"),
-                                       request.getParameter("group"),
-                                       request.getParameter("faculty"));
-        students.add(student);
-    }
-    
-    response.sendRedirect("/student.jsp");
-    
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AddStudent</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet AddStudent at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
